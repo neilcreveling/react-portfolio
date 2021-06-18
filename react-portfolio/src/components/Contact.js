0c8bc89cad0e6ef9230c1db222830d5339435e32
Neil Creveling <neil.creveling@gmail.com>
2021-06-18 13:36:49 -0400

    create Contact.js

---
+++ react-portfolio/src/components/Contact.js
@@ -0,0 +1,15 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+<Jumbotron>
+    <Container>
+        <h1>Contact Me</h1>
+        <ul>
+                <li><a href="tel:1-585-478-8852">(585)-478-8852</a></li>
+                <li><a href="https://github.com/neilcreveling">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/neil-creveling-9007a7131/">LinkedIn</a></li>
+                <li><a href="CrevelingResume_03_2021.pdf">Resume</a></li>
+        </ul>
+    </Container>
+</Jumbotron>
