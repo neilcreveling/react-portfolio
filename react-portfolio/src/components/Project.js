d1c8311476e3602b65b03ccbd75a2c62bcf39868
Neil Creveling <neil.creveling@gmail.com>
2021-06-18 14:30:47 -0400

    create Project.js

---
+++ react-portfolio/src/components/Project.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+function Project(props) {
+    console.log(props);
+
+    const mapDetails = () => {
+        console.log(props);
+        return props.details.map( (detail, j) => (
+        <li key={j}>{ detail }</li>
+        ));
+    };
+
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src={props.image} alt={props.name} />
+            <Card.Body>
+                <Card.Title>{props.name}</Card.Title>
+                <Card.Text>
+                    { mapDetails() }
+                    <Card.Link href={props.link} target="_blank" rel="noopener noreferrer">Link</Card.Link>
+                    <Card.Link href={props.github} target="_blank" rel="noopener noreferrer">GitHub</Card.Link>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default Project;
