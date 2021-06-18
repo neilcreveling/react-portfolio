cb0158880bf73975093ab99dd410ca63d08eaa4d
Neil Creveling <neil.creveling@gmail.com>
2021-06-18 14:38:47 -0400

    refactor Work.js so it works dynamically with projects and Project.js

--- react-portfolio/src/components/Work.js
+++ react-portfolio/src/components/Work.js
@@ -1,16 +1,39 @@
 import React from "react";
+import Project from "./Project"
+import projects from "../projects"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Jumbotron from 'react-bootstrap/Jumbotron';
 
-function Work() {
-    return (
-        <Jumbotron>
-    <Container>
-        <h1>Work</h1>
-         <Project />
-    </Container>
-</Jumbotron>
-    )
-}
+
+
+class Work extends Component {
+    state = {
+        projects
+    };
+
+    mapProjects = () => this.state.projects.length
+        ? this.state.projects.map( project => (
+            <Project
+                key={project.id}
+                name={project.name}
+                image={project.image}
+                details={project.details}
+                github={project.github}
+                link={project.link}
+            />
+        ))
+        : null
+
+    render() {
+        return (
+            <Jumbotron>
+            <Container>
+                <h1>Work</h1>
+                 <Project />
+            </Container>
+        </Jumbotron>
+        )
+    }
+};
 
 export default Work;
