2be3f5c1742c86bbdf6a22656a292a4ab3dbfa53
Neil Creveling <neil.creveling@gmail.com>
2021-06-18 13:41:37 -0400

    create Home.js

---
+++ react-portfolio/src/components/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+
+<Jumbotron id="homepage">
+    <Container>
+        <img src="images/neil_1.jpeg" alt="Neil Creveling Portrait"/>
+        <h1>Hi, I'm Neil.</h1>
+        <h2>I'm a full stack developer from Brooklyn, NY.</h2>
+        <h2>Learn more about me and what I do.</h2>
+    </Container>
+</Jumbotron>
